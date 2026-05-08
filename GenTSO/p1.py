ca6e23d529833417757658f9b0131d65ad7fffa9
Jayshil <jayshilpatel147@gmail.com>
2026-05-09 00:39:00 +0200

    also saving the bin width

--- GenTSO/p1.py
+++ GenTSO/p1.py
@@ -20,7 +20,7 @@ obs_wl, obs_depth, obs_error, band_widths = jwst.simulate_tso(
 
 fig, axs = plt.subplots(figsize=(16/1.5, 9/1.5))
 axs.plot(tso['wl'], tso['depth_spectrum']*1e6, c='salmon', label='depth at instrumental resolution')
-axs.errorbar(obs_wl, obs_depth*1e6, yerr=obs_error*1e6, fmt='o', ms=5, color='xkcd:blue', mfc=(1,1,1,0.85), label='simulated (noised up) transit spectrum')
+axs.errorbar(obs_wl, obs_depth*1e6, xerr=band_widths, yerr=obs_error*1e6, fmt='o', ms=5, color='xkcd:blue', mfc=(1,1,1,0.85), label='simulated (noised up) transit spectrum')
 
 axs.set_ylim([100, 650])
 
@@ -31,4 +31,4 @@ plt.legend(loc='best')
 plt.show()
 
 # -------- Saving the data --------
-np.savetxt(os.getcwd() + f'/GenTSO/Data/toi396b_transit_spectrum_isothermal_{instrument}_R250.txt', np.array([obs_wl, obs_depth*1e6, obs_error*1e6]).T, header='Wavelength [micron], Transit depth [ppm], Transit depth error [ppm]')
+np.savetxt(os.getcwd() + f'/GenTSO/Data/toi396b_transit_spectrum_isothermal_{instrument}_R250.txt', np.array([obs_wl, band_widths, obs_depth*1e6, obs_error*1e6]).T, header='Wavelength [micron], Band width [micron], Transit depth [ppm], Transit depth error [ppm]')
